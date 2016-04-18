063598d46bf9993ac3c98a6cd4b3c119c7ebc1b7
David <david.o.acker@gmail.com>
2016-04-18 09:52:47 -0400

    Attempting new PQ

--- SPOJ/haskell/PriorityQueue.hs
+++ SPOJ/haskell/PriorityQueue.hs
@@ -1,55 +1,53 @@
 -- Priority Queue Data Structure
 -- Author: David Ackerman
--- Apr 17, 2016
+-- Apr 18, 2016
+-- Another attempt, simpler data structures, better rank
 
-module PriorityQueue (PriorityQueue(EmptyQueue,Leaf,Branch,Node),peep,pop,insert,depth,toList,merge) where
+module PriorityQueue (PriorityQueue(EmptyQueue),readMin,insert,removeMin) where
 
-data PriorityQueue k v = 
+data PriorityQueue k v d = 
           EmptyQueue
-        | Leaf k v 
-        | Branch (PriorityQueue k v) k v 
-        | Node (PriorityQueue k v) (PriorityQueue k v) k v
+        | Node (PriorityQueue k v d) (PriorityQueue k v d) k v d
 
-peep :: Ord k => PriorityQueue k v -> v
-peep EmptyQueue = error "Peeping into empty Queue!"
-peep (Leaf k v) = v
-peep (Branch _ k v) = v
-peep (Node _ _ k v) = v
+rank :: (Ord k, Integral d) => PriorityQueue k v (d,d) -> d
+rank EmptyQueue = 0
+rank (Node l r k v d) = fst d
 
-depth :: Ord k => PriorityQueue k v -> Int
+depth :: (Ord k, Integral d) => PriorityQueue k v (d,d) -> d
 depth EmptyQueue = 0
-depth (Leaf k v) = 1
-depth (Branch l k v) = 1 + depth l
-depth (Node l r k v) = 1 + depth l
+depth (Node l r k v d) = snd d
 
-toList :: Ord k => PriorityQueue k v -> [v]
-toList EmptyQueue = []
-toList (Leaf k v) = [v]
-toList (Branch l k v) = [v] ++ toList l
-toList (Node l r k v) = [v] ++ toList l ++ toList r
+readMin :: (Ord k, Integral d) => PriorityQueue k v (d,d) -> v
+readMin EmptyQueue = error "Reading EmptyQueue"
+readMin (Node l r k v d) = v
 
-insert :: Ord k => k -> v -> PriorityQueue k v -> PriorityQueue k v
-insert xk x EmptyQueue = Leaf xk x
-insert xk x (Leaf k v) 
-    | xk < k       = Branch (Leaf k v) xk x
-    | otherwise   = Branch (Leaf xk x) k v
-insert xk x (Branch l k v)
-    | xk < k       = Node l (Leaf k v) xk x
-    | otherwise   = Node l (Leaf xk x) k v
-insert xk x (Node l r k v)
-    | depth l > depth r = if k > xk then Node l (insert k v r) xk x
-                        else Node l (insert xk x r) k v
-    | otherwise         = if k > xk then Node (insert k v l) r xk x
-                        else Node (insert xk x l) r k v
+readMinK :: (Ord k, Integral d) => PriorityQueue k v (d,d) -> k
+readMinK EmptyQueue = error "Reading EmptyQueue"
+readMinK (Node l r k v d) = k
 
-pop :: Ord k => PriorityQueue k v -> PriorityQueue k v
-pop EmptyQueue = error "Popping empty Queue"
-pop (Leaf k v) = EmptyQueue
-pop (Branch l k v) = l
-pop (Node l r k v) = merge l r
+insert :: (Ord k, Integral d) => k -> v -> PriorityQueue k v (d,d) -> PriorityQueue k v (d,d)
+insert k' v' EmptyQueue = Node EmptyQueue EmptyQueue k' v' (1,1)
+insert k' v' (Node l r k v d)
+    |rr < rl        = Node oldNode newNode newK newV (newRank,newDepth)
+    |otherwise      = Node newNode oldNode newK newV (newRank,newDepth)
+                where
+                    rl = rank l
+                    rr = rank r
+                    oldNode = if rr < rl then l else r
+                    replacedNode = if rr < rl then r else l
+                    newNode = if k' < k then insert k v replacedNode else insert k' v' replacedNode
+                    newRank = 1 + (min (rank newNode) (rank oldNode))
+                    newDepth = 1 + (max (depth newNode) (depth oldNode))
+                    (newK, newV) = if k' < k then (k',v') else (k,v)
 
-merge :: Ord k => PriorityQueue k v -> PriorityQueue k v -> PriorityQueue k v
-merge nL EmptyQueue = nL
-merge nL (Leaf k v) = insert k v nL
-merge nL (Branch l k v) = insert k v (merge nL l)
-merge nL (Node l r k v) = insert k v (merge nL (merge l r))
+
+removeMin :: (Ord k, Integral d) => PriorityQueue k v (d,d) -> PriorityQueue k v (d,d)
+removeMin EmptyQueue = error "Removing from EmptyQueue"
+removeMin (Node l r k v d)
+    |takeRight,goRight   = Node l newNode (readMinK r) (readMin r) (min (fst d) (rank newNode), max (snd d) (depth newNode))
+    |
+    |otherwise                = Node l r k v d
+    where
+        goRight = (rank r < rank l) && (depth r <= depth l)
+        takeRight = readMin r <= readMin l
+
